94c14a05e38251acf713fd93eb3acb40b7ebc5e9
Александр <malcovic8@gmail.com>
2019-09-11 01:57:02 +0500

    start project

---
+++ CoffeeSharp/Compiler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeSharp
+{
+    class Compiler
+    {
+    }
+}
---
+++ CoffeeSharp/Lexer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+
+namespace CoffeeSharp
+{
+    class Lexer
+    {
+        private Tuple<string, string>[] tuples;
+
+        public Lexer(CompilationUnitSyntax root)
+        {
+
+        }
+
+
+    }
+}
